e3a6aaa6fd4b3afdc3f538b2291d28808005d8ac
atabek-bakh <coderonline@icloud.com>
2022-07-31 02:06:44 +0200

    cover image frame style

--- public/javascripts/fileUpload.js
+++ public/javascripts/fileUpload.js
@@ -8,6 +8,7 @@ FilePond.setOptions({
   stylePanelAspectRatio: 150 / 100,
   imageResizeTargetWidth: 100,
   imageResizeTargetHeight: 150,
+  imageResizeMode: 'cover',
 })
 
 FilePond.parse(document.body)
